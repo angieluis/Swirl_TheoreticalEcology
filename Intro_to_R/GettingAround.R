16eef4c4ee27d422d16ac86a12500301e1e9d8ef
angieluis <angela.d.luis@gmail.com>
2021-08-11 14:26:32 -0600

    remove old credentials

--- Intro_to_R/GettingAround.R
+++ Intro_to_R/GettingAround.R
@@ -1,10 +1,18 @@
-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
-
-library(gitcreds)
 
 
+
+
+
+# for me to turn into file that can be downloaded:
 set_lesson("Intro_to_R/Basic_Building_Blocks/lesson.yaml")
 pack_course()
+
+
+
+## For students:
+
+# first time
+install.packages("swirl")
 
 # setwd to where ever downloaded, then
 install_course(swc_path="Intro_to_R.swc")
