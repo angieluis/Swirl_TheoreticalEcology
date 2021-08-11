6c606e1eae8a1039503a19255ee736d5398b9389
angieluis <angela.d.luis@gmail.com>
2021-08-11 14:19:42 -0600

    initial

---
+++ Intro_to_R/GettingAround.R
@@ -0,0 +1,15 @@
+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
+
+library(gitcreds)
+
+
+set_lesson("Intro_to_R/Basic_Building_Blocks/lesson.yaml")
+pack_course()
+
+# setwd to where ever downloaded, then
+install_course(swc_path="Intro_to_R.swc")
+swirl()
+# what call you?
+
+# Choose 1: Intro to R
+
